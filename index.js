1e3c974a102f4b4a2380bd11816c9cd304bae3ec
dede53 <hallomarcel.hm@gmail.com>
2016-11-20 00:14:18 +0100

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+var child_process 			= require('child_process');
+var adapter 				= require('../../adapter-lib.js');
+var exec 					= new adapter({
+	"name": "Url",
+	"loglevel": 3,
+	"description": "Führt http-requests aus.",
+	"settingsFile": ""
+});
+
+process.on('message', function(data) {
+	var status = data.status;
+	var data = data.data;
+	sendEXEC(status, data);
+});
+
+
+function sendEXEC(status, data){
+	if(status == 1){
+		var execString = data.CodeOn;
+	}else{
+		var execString = data.CodeOff;
+	}
+	child_process.exec(execString, function(error, stdout, stderr){
+		if(error){
+			exec.log.error(error);
+		}else{
+			exec.log.debug(stdout);
+			exec.log.debug("Executing Done");
+		}
+	});
+}
